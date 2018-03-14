ce84e8f26e056d5364d9d62c622fd2b867fb8748
Kathy Walker <kjata1013@gmail.com>
2018-03-14 11:15:03 -0400

    writing DALs

---
+++ NPGeek.Web/DAL/IParksDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPGeek.Web.Models;
+using System.Data.SqlClient;
+
+namespace NPGeek.Web.DAL
+{
+	public class IParksDAL
+	{
+		private string connectionString;
+
+		public IParksDAL(string dbConnectionString)
+		{
+			connectionString = dbConnectionString;
+		}
+
+
+		
+
+	}
+}
---
+++ NPGeek.Web/DAL/ParkSqlDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NPGeek.Web.Models;
+using System.Data.SqlClient;
+
+namespace NPGeek.Web.DAL
+{
+	public class ParkSqlDAL : IParksDAL
+	{
+		const string connectionString = @"Server =.\SQLEXPRESS; database = npgeek; Trusted_Connection = True;";
+
+		public List<Park> GetParks(string code)
+		{
+			List<Park> parks = new List<Park>();
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					conn.Open();
+					SqlCommand cmd = new SqlCommand(@"SELECT * FROM parks;", conn);
+					SqlDataReader reader = cmd.ExecuteReader();
+
+				}
+			}
+			
+
+			private static Park MapRowToPark(SqlDataReader reader)
+			{
+				return new Park
+				{
